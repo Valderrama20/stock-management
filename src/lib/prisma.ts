9a715097a3b3a8d58f207a1270a0b7176c03a937
valderrama20 <jose23122009@gmail.com>
2025-07-30 19:51:37 +0200

    fix: Corregir la importación de PrismaClient en el archivo prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   // Extendemos globalThis para evitar múltiples instancias en dev
